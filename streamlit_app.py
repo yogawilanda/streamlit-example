6fbfc55f36e628d061115d27e8cf33a8a43a1891
E A Yoga Wilanda <hyoga.wilanda@gmail.com>
2024-05-12 15:33:26 +0700

    Added Title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
+st.title("Local AI With Streamlit")
+
 with st.sidebar:
-    messages = st.container(height=300)
+    messages = st.container(height=800)
     if prompt := st.chat_input("Say something"):
         messages.chat_message("user").write(prompt)
         messages.chat_message("assistant").write(f"Echo: {prompt}")
